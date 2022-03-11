d5e418c59b5b83f4d8118643de220a97fabda206
Newman <newman.kcchow@gmail.com>
2022-03-11 08:54:28 +0800

    Setup strapi client

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
-    <h1 class="bg-red-500">Hello</h1>
+    <h1 class="bg-green-500">Hello {{ message }}</h1>
+    <input v-model="message" />
+    <textarea :value="$md.render(data, null, 4)" />
   </div>
 </template>
+
+<script setup lang="ts">
+const strapi = useStrapi4();
+const message = ref("A");
+
+const { data } = await useAsyncData('posts', () => strapi.find('posts', { populate: ["content"] }))
+
+onMounted(() => {
+  strapi.find('posts')
+})
+</script>
